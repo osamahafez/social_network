00e51d2647e34f1db2aceab870262ceedd19b373
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-06-28 14:36:55 +0200

    Add current profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport');
+
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+
 
 // @route GET api/profile
-// @access Public
-router.get('/', (req, res) => {
-    res.json({
-        msg: "profile Page"
-    });
-})
+// @desc get user's profile page
+// @access Private
+router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+
+    Profile.findOne({user: req.user._id})
+        .then(profile => {
+            
+            let errors = {};
+
+            if(!profile) {
+                errors.noprofile = 'Profile not found'
+                return res.status(404).json(errors);
+            }
+
+            return res.status(200).json(profile);
+
+        })  
+        .catch(err => res.json(err));
+});
+
 
 module.exports = router;
