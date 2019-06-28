c6e7f0987ba3f6bc9066cb157dab2038af5603a4
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-06-28 14:36:14 +0200

    Add Profile model

---
+++ models/Profile.js
@@ -0,0 +1,117 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const profileSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    handle: {
+        type: String,
+        required: true
+    },
+    company: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    status: {
+        type: String
+    },
+    skills: {
+        type: [String]
+    },
+    bio: {
+        type: String
+    },
+    github: {
+        type: String
+    },
+    experience: [
+        {
+            title: {
+                type: String,
+                required: true
+            },
+            description: {
+                type: String
+            },
+            company: {
+                type: String,
+                required: true
+            },
+            location: {
+                type: String
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: String
+            },
+            current: {
+                type: Boolean,
+                default: false
+            }
+        }
+    ],
+    education: [
+        {
+            school: {
+                type: String,
+                required: true
+            },
+            degree: {
+                type: String,
+                required: true
+            },
+            field: {
+                type: String,
+                required: true
+            },
+            location: {
+                type: String
+            },
+            from: {
+                type: Date,
+                required: true
+            },
+            to: {
+                type: String
+            },
+            current: {
+                type: Boolean,
+                default: false
+            }
+        }
+    ],
+    social: [
+        {
+            youtube: {
+                type: String
+            },
+            facebook: {
+                type: String
+            },
+            twitter: {
+                type: String
+            },
+            linkedin: {
+                type: String
+            },
+            instagram: {
+                type: String
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Profile = mongoose.model('Profile', profileSchema);
+
+module.exports = Profile;
