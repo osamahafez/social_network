8ad036f6feab7c69fb7c06a3bebd94c7a8394558
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-07-06 15:17:21 +0200

    Configuring react-router-dom into the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
-
+import Footer from './components/layout/Footer';
+import { Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Navbar />
+            <Route exact path='/' component={Landing} />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
