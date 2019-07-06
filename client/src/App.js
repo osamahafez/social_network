f59db418152601749d8319a5e85e3596f074700f
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-07-06 22:04:53 +0200

    Add route and link components to front-end

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import { Route } from 'react-router-dom';
 
 function App() {
@@ -10,6 +12,12 @@ function App() {
         <div className='App'>
             <Navbar />
             <Route exact path='/' component={Landing} />
+
+            <div className='container'>
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
+            </div>
+
             <Footer />
         </div>
     );
