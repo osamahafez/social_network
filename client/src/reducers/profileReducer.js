ff4a3619ddadc06eb5b130a74d481b6571622eb4
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-07-17 23:27:55 +0200

    create profile reducer

---
+++ client/src/reducers/profileReducer.js
@@ -0,0 +1,30 @@
+import { PROFILE_LOADING, GET_PROFILE } from '../actions/types';
+
+const initialState = {
+    profile: null,
+    profiles: null,
+    loading: false
+};
+
+const profileReducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        case PROFILE_LOADING:
+            return {
+                ...state,
+                loading: true
+            };
+
+        case GET_PROFILE:
+            return {
+                ...state,
+                profile: action.payload,
+                loading: false
+            };
+
+        default:
+            return state;
+    }
+};
+
+export default profileReducer;
