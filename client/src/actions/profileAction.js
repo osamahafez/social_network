8103b7b0f7449fc2dcfe3f5f0201121f3261c62f
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-07-17 23:27:31 +0200

    create profile actions

---
+++ client/src/actions/profileAction.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { PROFILE_LOADING, GET_PROFILE } from './types';
+
+// get user's profile
+export const getCurrentProfile = () => (dispatch) => {
+    dispatch(setProfileLoading());
+    axios
+        .get('/api/profile')
+        .then((res) => {
+            dispatch({
+                type: GET_PROFILE,
+                payload: res.data
+            });
+        })
+        .catch((err) => {
+            dispatch({
+                type: GET_PROFILE,
+                payload: {} // return empty object to tell the user that he didn't create a profile yet
+            });
+        });
+};
+
+// set loading
+export const setProfileLoading = () => {
+    return {
+        type: PROFILE_LOADING
+    };
+};
