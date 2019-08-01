ca805ba2d4fb9be990de9cedb619d626a0b71df4
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-08-01 20:08:02 +0200

    create getProfileByHandle action

--- client/src/actions/profileAction.js
+++ client/src/actions/profileAction.js
@@ -27,6 +27,25 @@ export const getCurrentProfile = () => (dispatch) => {
         });
 };
 
+// get profile using user's handle
+export const getProfileByHandle = (handle) => (dispatch) => {
+    dispatch(setProfileLoading());
+    axios
+        .get(`/api/profile/handle/${handle}`)
+        .then((res) =>
+            dispatch({
+                type: GET_PROFILE,
+                payload: res.data
+            })
+        )
+        .catch(() =>
+            dispatch({
+                type: GET_PROFILE,
+                payload: null
+            })
+        );
+};
+
 // create user's profile
 export const createProfile = (profileData, history) => (dispatch) => {
     axios
