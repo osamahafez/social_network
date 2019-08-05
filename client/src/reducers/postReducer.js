b50288c17fea6d0afb546c8c7ce9d1f751e5b39f
Osama Hafez <Eng.Osama.M.Hafez@gmail.com>
2019-08-06 01:08:38 +0200

    add GET_POST case

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,4 +1,10 @@
-import { ADD_POST, GET_POSTS, GET_POST, POST_LOADING, DELETE_POST } from '../actions/types';
+import {
+    ADD_POST,
+    GET_POSTS,
+    GET_POST,
+    POST_LOADING,
+    DELETE_POST
+} from '../actions/types';
 
 const initialState = {
     posts: [],
@@ -8,20 +14,26 @@ const initialState = {
 
 const postReducer = (state = initialState, action) => {
     switch (action.type) {
-
         case POST_LOADING:
             return {
                 ...state,
                 loading: true
-            }
-        
+            };
+
         case GET_POSTS:
             return {
                 ...state,
                 posts: action.payload,
                 loading: false
-            }
-        
+            };
+
+        case GET_POST:
+            return {
+                ...state,
+                post: action.payload,
+                loading: false
+            };
+
         case ADD_POST:
             return {
                 ...state,
@@ -31,8 +43,8 @@ const postReducer = (state = initialState, action) => {
         case DELETE_POST:
             return {
                 ...state,
-                posts: state.posts.filter(post => post._id !== action.payload)
-            }
+                posts: state.posts.filter((post) => post._id !== action.payload)
+            };
         default:
             return state;
     }
